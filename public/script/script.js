508fe8477fc50715f0d539a66f3bada3fe810dd2
lelienhuong <huonglien212@gmail.com>
2020-11-08 16:59:32 +0700

    updated

--- public/script/script.js
+++ public/script/script.js
@@ -1,28 +1,39 @@
-var input = document
-    .getElementById('input-url')
-input.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("submit-url").click();
-    }
-});
+// returnID = () => {
+//     var x = window.location.href;
+//     var y = x;
+//     var z;
+//     while (y.indexOf("/") !== -1) {
+//         z = y.indexOf("/")
+//         y = y.replace("/", ".");
+//     }
+//     z++;
+//     var t = x.substr(z, x.length)
+//     if (t.length != 0) {
+//         window.location.href = "https://dsc-dut.herokuapp.com/" + t;
+//     }
+// }
 
-checkNull = () => {
-    var inputLink = document.getElementById('input-url').value;
+
+checkFormat=(myURL)=>{
+    var pattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)+[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
+            return pattern.test(myURL);
+}
+checkNull = (inputLink) => {
+//    var inputLink = document.getElementById('input-url').value;
     inputLink = inputLink.trim();
     if (inputLink.indexOf(" ") !== -1 || inputLink.length == 0) {
         alert("Please input your link again!")
     } else {
-        var x = document.getElementById('shorten-output');
-        if (x.style.display === 'none') {
-            x.style.display = 'block';
-        } else {
-            x.style.display = 'none';
-            window.location.reload();
-        }
+        // var x = document.getElementById('shorten-output');
+        // if (x.style.display === 'none') {
+        //     x.style.display = 'block';   
+        // } else {
+        //     x.style.display = 'none';
+        //     window.location.reload();
+        // }
 
         var slug = '';
-        window.fetch("/url", {
+        window.fetch("https://dsc-dut.herokuapp.com/url", {
             method: 'POST',
             body: JSON.stringify({ "url": inputLink }),
             headers: {
@@ -31,14 +42,24 @@ checkNull = () => {
         })
             .then(res => res.json())
             .then(response => slug = response.slug)
-            .then(response => document.getElementById('output-url').value = window.location.href + slug)
+            .then(response => document.getElementById('output-url').value = "https://dsc-dut.herokuapp.com/" + slug)
             .catch(error => console.error('Error:', error))
     }
 }
 document
-    .getElementById('submit-url')
-    .addEventListener('click', function () {
-        checkNull();
+    .getElementById('form-submit')
+    .addEventListener('submit', function () {
+        event.preventDefault();
+        var inputLink = document.getElementById('input-url').value;
+        if(checkFormat(inputLink)){
+            checkNull(inputLink);
+        }else{
+            if(checkFormat("https://"+inputLink)){
+                checkNull("https://"+inputLink);
+            }else{
+                alert("Please input your link again!");
+            }
+        }
     }
     )
     
