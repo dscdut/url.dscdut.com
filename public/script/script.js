fb34daccc74950fb9434157c80eb1d9687c05763
lelienhuong <huonglien212@gmail.com>
2020-11-09 19:53:18 +0700

    updated

--- public/script/script.js
+++ public/script/script.js
@@ -1,37 +1,8 @@
-// returnID = () => {
-//     var x = window.location.href;
-//     var y = x;
-//     var z;
-//     while (y.indexOf("/") !== -1) {
-//         z = y.indexOf("/")
-//         y = y.replace("/", ".");
-//     }
-//     z++;
-//     var t = x.substr(z, x.length)
-//     if (t.length != 0) {
-//         window.location.href = "https://dsc-dut.herokuapp.com/" + t;
-//     }
-// }
-
-
 checkFormat = (myURL) => {
     var pattern = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)+[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
     return pattern.test(myURL);
 }
 checkNull = (inputLink) => {
-    //    var inputLink = document.getElementById('input-url').value;
-    // inputLink = inputLink.trim();
-    // if (inputLink.indexOf(" ") !== -1 || inputLink.length == 0) {
-    //     alert("Please input your link again!")
-    // } else {
-    // var x = document.getElementById('shorten-output');
-    // if (x.style.display === 'none') {
-    //     x.style.display = 'block';   
-    // } else {
-    //     x.style.display = 'none';
-    //     window.location.reload();
-    // }
-
     var slug = '';
     window.fetch("https://dsc-dut.herokuapp.com/url", {
         method: 'POST',
