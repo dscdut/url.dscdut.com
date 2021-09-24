293cd1b8028f308db488aacb0ddbcf89f4793c1a
Kim Chi <kimchilethi411@gmail.com>
2021-09-24 16:47:55 +0700

    feat(8): add create user api
    - using bcrypt to hash password

--- src/backend/modules/user/user.service.js
+++ src/backend/modules/user/user.service.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { UserRepository } = require('./user.repository');
 const { DuplicateException } = require('../../common/httpException');
 const UserModel = require('../../model/user.model');
@@ -9,6 +10,8 @@ class Service {
 
     async createOne(userDto) {
         const isUserExist = await this.repository.findByEmail(userDto.email);
+        const hashStr = bcrypt.genSaltSync(10);
+        userDto.password = bcrypt.hashSync(userDto.password, hashStr);
         if (isUserExist) {
             throw new DuplicateException(`User (${userDto.email}) is already existed`);
         }
