48a8fbb8245913ccb36aadee03b14b16608514fb
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-10-04 00:12:35 +0700

    feat(31): design 404 page

--- src/backend/routes/view.route.js
+++ src/backend/routes/view.route.js
@@ -5,6 +5,7 @@ const { UrlController } = require('../api/url/url.controller');
 
 router.get('/', (req, res) => res.render('index'));
 router.get('/myurls', (req, res) => res.render('myurls'));
+router.get('/not-found', (req, res) => res.render('not-found'));
 
 router.get('/:slug', UrlController.findBySlug);
 
