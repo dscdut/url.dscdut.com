de0ca059afb13dc2246e2989baf866abef20578a
ichhoa <ichhoa129@gmail.com>
2021-10-06 22:48:21 +0700

    feat(49): add get user click information
    - Add createdAt in Url model

--- src/backend/modules/url/url.model.js
+++ src/backend/modules/url/url.model.js
@@ -9,13 +9,20 @@ module.exports = class UrlModel {
 
     visitors;
 
+    createdAt;
+
+    constructor() {
+        this.createdAt = new Date();
+    }
+
     toJson() {
         return {
             url: this.url,
             isCustom: this.isCustom,
             slug: this.slug,
             userId: this.userId,
-            visitors: this.visitors
+            visitors: this.visitors,
+            createdAt: this.createdAt
         };
     }
 };
