d7d09bd6832a49628d1395c20ac8eafb5f1fdd9e
ichhoa <ichhoa129@gmail.com>
2021-10-24 03:58:31 +0700

    feat(78): add search url api
    - Add search url with absolute slug
    - Will change to search by url keywords later

--- src/backend/api/url/url.router.js
+++ src/backend/api/url/url.router.js
@@ -6,12 +6,12 @@ const { CreateUrlInterceptor } = require('@modules/url/interceptor/createUrl.int
 const { DeleteUrlsInterceptor } = require('@modules/url/interceptor/deleteUrls.interceptor');
 const { UpdateUrlInterceptor } = require('@modules/url/interceptor/updateUrl.interceptor');
 const { AuthRequired } = require('@modules/auth/guard/authRequired');
-const { CheckQueryInterceptor } = require('@modules/url/interceptor/checkQuery.interceptor');
+const { QueryInterceptor } = require('@modules/url/interceptor/query.interceptor');
 const { UrlController } = require('./url.controller');
 
 router.post('/', CreateUrlInterceptor.intercept, UrlController.createOne);
 router.delete('/', AuthRequired(), DeleteUrlsInterceptor.intercept, UrlController.deleteMany);
 router.put('/:id', AuthRequired(), UpdateUrlInterceptor.intercept, UrlController.updateOne);
-router.get('/', AuthRequired(), CheckQueryInterceptor.intercept, UrlController.findAll);
+router.get('/', AuthRequired(), QueryInterceptor.intercept, UrlController.findAll);
 
 module.exports.urlRouter = router;
--- src/backend/modules/url/interceptor/query.interceptor.js
+++ src/backend/modules/url/interceptor/query.interceptor.js
@@ -1,9 +1,10 @@
 const Joi = require('joi');
 const { DefaultValidatorInterceptor } = require('@infrastructure/interceptor/default-validator.interceptor');
 
-module.exports.CheckQueryInterceptor = new DefaultValidatorInterceptor(
+module.exports.QueryInterceptor = new DefaultValidatorInterceptor(
     Joi.object({
         page: Joi.number().min(1),
         limit: Joi.number(),
+        search: Joi.string().allow(null, '')
     })
 );
