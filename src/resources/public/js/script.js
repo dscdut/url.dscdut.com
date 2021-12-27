31e9b4fba93d9577a7b5694f053493a7131b7f5e
Huynh Thi Khanh Linh <73637868+bongudth@users.noreply.github.com>
2021-12-27 17:59:40 +0700

    feat(61): add responsive to ui (#97)

--- src/resources/public/js/script.js
+++ src/resources/public/js/script.js
@@ -158,7 +158,7 @@ function onSuccess(googleUser) {
 function renderButton() {
   gapi.signin2.render('my-signin2', {
     'scope': 'profile email',
-    'width': 240,
+    'width': 'auto',
     'height': 50,
     'longtitle': true,
     'theme': 'dark',
