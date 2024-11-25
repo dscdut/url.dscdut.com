394aebc7256be8e11d1fce26f1a53f3b9799a3a7
High10Hunter <trandaiquy2903@gmail.com>
2024-11-25 19:30:20 +0700

    feat: update Firebase service key configuration to use environment variable

--- src/backend/database/index.js
+++ src/backend/database/index.js
@@ -1,8 +1,13 @@
 const firebase = require('firebase-admin');
-const serviceKey = require('./service_key.json');
+const { SERVICE_KEY_PATH } = require('@env');
+
+// const serviceKey = require('./service_key.json');
+// The secret file is placed in the root directory of the project by default in Render
+// ref: https://render.com/docs/configure-environment-variables#secret-files
+const serviceKey = require(SERVICE_KEY_PATH);
 
 const db = firebase.initializeApp({
-    credential: firebase.credential.cert(serviceKey),
+	credential: firebase.credential.cert(serviceKey),
 });
 
 db.firestore().settings({ ignoreUndefinedProperties: true });
--- src/backend/env/index.js
+++ src/backend/env/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    CORS_ALLOW: process.env.CORS_ALLOW,
-    SESSION_SECRET: process.env.SESSION_SECRET,
-    SALT_ROUNDS: process.env.SALT_ROUNDS || 10,
-    CLIENT_ID: process.env.CLIENT_ID,
-    JWT_SECRET: process.env.JWT_SECRET || 'hehe',
-    EXPIRE_DAYS: process.env.EXPIRE_DAYS || '1d',
-    SITE_KEY: process.env.SITE_KEY,
-    SECRET_KEY: process.env.SECRET_KEY
+	PORT: process.env.PORT || 8000,
+	CORS_ALLOW: process.env.CORS_ALLOW,
+	SESSION_SECRET: process.env.SESSION_SECRET,
+	SALT_ROUNDS: process.env.SALT_ROUNDS || 10,
+	CLIENT_ID: process.env.CLIENT_ID,
+	JWT_SECRET: process.env.JWT_SECRET || 'hehe',
+	EXPIRE_DAYS: process.env.EXPIRE_DAYS || '1d',
+	SITE_KEY: process.env.SITE_KEY,
+	SECRET_KEY: process.env.SECRET_KEY,
+	SERVICE_KEY_PATH: process.env.SERVICE_KEY_PATH || './service_key.json',
 };
